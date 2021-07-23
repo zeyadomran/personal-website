61aa6026057392d6eaf91a5e0a37c6fa2cdde9bb
zeyadomran <ziomran@gmail.com>
2021-07-23 22:09:56 +0400

    Added SCS Project

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -14,23 +14,23 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
 	return (
 		<div
 			ref={ref}
-			className="flex flex-col justify-between items-center p-4 w-full rounded-lg bg-dark-blue text-white hover:-translate-y-1 transition duration-300 ease-in-out"
+			className="flex flex-col items-center justify-between p-4 w-full rounded-lg bg-dark-blue text-white hover:-translate-y-1 transition duration-300 ease-in-out"
 		>
-			<div className="flex items-center justify-between w-full mb-1">
+			<div className="flex items-center justify-between w-full">
 				<h3 className="font-heading text-3xl text-white font-bold">
 					{project.name}
 				</h3>
 				<Badge>{project.year}</Badge>
 			</div>
-			<div className="flex flex-row items-left justify-start flex-wrap w-full mb-2">
+			<div className="flex flex-row items-left justify-start flex-wrap w-full mb-1">
 				{project.technologies.map((name, index) => (
 					<Badge key={index}>{name}</Badge>
 				))}
 			</div>
-			<p className="text-xl text-white text-left font-body mb-2">
+			<p className="text-xl text-white text-left font-body mb-1">
 				{project.description}
 			</p>
-			<CTAButton href={project.url}>View Source Code</CTAButton>{" "}
+			<CTAButton href={project.url}>View Source Code</CTAButton>
 		</div>
 	);
 };
